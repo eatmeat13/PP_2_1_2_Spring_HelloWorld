23c2a239c10bb59a06b1295155df97c057b87d4e
Myasoedov <myasoedov.leon@mail.ru>
2022-03-22 14:51:26 +0300

    commit1

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -7,8 +7,16 @@ public class AppConfig {
  
     @Bean(name="helloworld")
     public HelloWorld getHelloWorld() {
-        HelloWorld helloWorld = new HelloWorld();
-        helloWorld.setMessage("Hello World!");
-        return helloWorld;
+        HelloWorld helloWorld1 = new HelloWorld();
+        helloWorld1.setMessage("Hello World!");
+        return helloWorld1;
+    }
+
+    @Bean(name="cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setMessage("Мяу");
+        return cat;
     }
 }
